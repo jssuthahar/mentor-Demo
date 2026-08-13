94a40b255680305acf7d3ff0f4fc29314427c3ff
Suthahar <jssuthahar@gmail.com>
2026-08-13 22:36:57 +0800

    Added Login and Register screen

---
+++ mentor/mentor/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mentor.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterPost()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+    }
+}
